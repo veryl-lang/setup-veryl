591f8c366dd0b3c58dd358350c91fc57512ea812
dalance <dalance@gmail.com>
2024-09-27 10:52:20 +0900

    Remove cache

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const core = require('@actions/core');
 const tc = require('@actions/tool-cache');
-const octo = require("@octokit/rest");
 const { getDownloadObject } = require('./lib/utils');
 
 async function setup() {
@@ -9,38 +8,16 @@ async function setup() {
     // Get version of tool to be installed
     const version = core.getInput('version');
 
-    // Get latest version
-    const octokit = new octo.Octokit();
-    const resp = await octokit.request('GET /repos/veryl-lang/veryl/releases/latest', {
-      owner: 'veryl-lang',
-      repo: 'veryl',
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28'
-      }
-    });
-    const latest = resp['name'];
-    const resolvedVersion = version === 'latest' ? latest : version;
-    core.info(`resolved version: ${ resolvedVersion }`);
+    // Download the specific version of the tool, e.g. as a tarball/zipball
+    const download = getDownloadObject(version);
+    const pathToTarball = await tc.downloadTool(download.url);
 
-    let toolPath = tc.find('veryl', resolvedVersion);
-
-    if (!toolPath) {
-      core.info('downloading Veryl binary');
-
-      // Download the specific version of the tool, e.g. as a tarball/zipball
-      const download = getDownloadObject(resolvedVersion);
-      const pathToTarball = await tc.downloadTool(download.url);
-
-      // Extract the tarball/zipball onto host runner
-      const extract = download.url.endsWith('.zip') ? tc.extractZip : tc.extractTar;
-      const extractedPath = await extract(pathToTarball);
-      toolPath = await tc.cacheDir(extractedPath, 'veryl', resolvedVersion);
-    } else {
-      core.info('use cached binary');
-    }
+    // Extract the tarball/zipball onto host runner
+    const extract = download.url.endsWith('.zip') ? tc.extractZip : tc.extractTar;
+    const pathToCLI = await extract(pathToTarball);
 
     // Expose the tool by adding it to the PATH
-    core.addPath(toolPath);
+    core.addPath(path.join(pathToCLI, download.binPath));
   } catch (e) {
     core.setFailed(e);
   }
